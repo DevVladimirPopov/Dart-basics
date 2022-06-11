3a1b42dc8da96692049c4592e04d8ea8264ef253
DevVladimirPopov <107247047+DevVladimirPopov@users.noreply.github.com>
2022-06-11 18:24:24 +0100

    DZ 2.3 done

--- main.dart
+++ main.dart
@@ -1,7 +1,6 @@
 void main() {
 // ДЗ 2.1 вычисления НОД и НОК целых чисел.
   print("ДЗ ---->  № 2.1");
-  print('-' * 50);
 
   int fst = 340;
   int sec = 33;
@@ -12,13 +11,12 @@ void main() {
   print('1е число = $fst / 2е Число = $sec');
   print('Наибольший общий делитель НОД = $nod1');
   print('Наименьшее общее кратное НОК = $nok1');
-  print('-' * 50);
+  print('*' * 50);
 // -----------------------------------------
 
 // 2.2 Преобразования целых чисел из десятичной
 // системы в двоичную и обратно.
   print("ДЗ ---->  № 2.2");
-  print('-' * 50);
 
   int dig1 = 455;
   String str1 = "1111100111";
@@ -29,15 +27,15 @@ void main() {
   print("Число $dig1  == $result1 (2й системе)");
   print("Число $str1 == $result2 (10й системе)");
 
-  print('-' * 50);
+  print('*' * 50);
 // -----------------------------------------
 
 // 2.3 Реализуйте метод, который принимает строку слов, разделённых пробелами.
 //Задача — найти в данной строке числа и вернуть коллекцию num этих чисел.
   print("ДЗ ---->  № 2.3");
-  print('-' * 50);
-
-  print('-' * 50);
+  var result = tryParse("Тестовая строка 5 чиcла тут есть 20 наверное )");
+  print('Числа разделенные пробелами в исходной строке = $result');
+  print('*' * 50);
 // -----------------------------------------
 }
 
@@ -68,3 +66,30 @@ int binaryToNumber(digits) => int.parse(digits, radix: 2);
 
 // Преобразования целых чисел из двоичной системы в десятичную.
 String NumberToBinary(digits) => digits.toRadixString(2);
+
+/*
+searchNum(String str) {
+  List<num> result = [1, 2];
+  List<String> list = str.split('');
+  print(list);
+
+  for (String c in list) {
+    if (c == null) {
+      print("$c = число");
+    }
+  }
+  return result;
+}
+*/
+tryParse(String input) {
+  List<String> source = input.trim().split(" ");
+  List<int> result = [];
+  int x = 0;
+  for (String c in source) {
+    if (int.tryParse(c) != null) {
+      x = int.parse(c);
+      result.add(x);
+    }
+  }
+  return result;
+}
